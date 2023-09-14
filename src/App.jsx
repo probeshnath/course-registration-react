6a880cb834dfb67f4baeb64f1ca369ae4f8a5537
probeshnath <probeshn@gmail.com>
2023-09-14 23:15:34 +0600

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { FaBeer } from 'react-icons/fa';
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-2xl text-center text-orange-500'>Hi , We are start our Assignment-7 on Course Registration<FaBeer /> </h1>
+    </>
+  )
+}
+
+export default App
